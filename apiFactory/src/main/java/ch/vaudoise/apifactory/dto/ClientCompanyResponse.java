13449d2afebfa1b4560ef65f44a22f2fcbba2f76
chiheb bidani <bidani.chiheb@yahoo.fr>
2025-10-15 14:44:31 +0200

    Add listOfContracts field to ClientCompanyResponse and ClientPersonResponse for contract management

--- apiFactory/src/main/java/ch/vaudoise/apifactory/dto/ClientCompanyResponse.java
+++ apiFactory/src/main/java/ch/vaudoise/apifactory/dto/ClientCompanyResponse.java
@@ -1,13 +1,17 @@
 package ch.vaudoise.apifactory.dto;
 
+import ch.vaudoise.apifactory.entities.Contract;
+
 import java.util.Date;
+import java.util.List;
 
 public record ClientCompanyResponse (
         Long id,
         String name,
         String email,
         String type,
-        String companyIdentifier            // For COMPANY
+        String companyIdentifier,            // For COMPANY
+        List<ContractRequest> listOfContracts
 
 ) implements ClientResponse {
 }
--- apiFactory/src/main/java/ch/vaudoise/apifactory/dto/ClientPersonResponse.java
+++ apiFactory/src/main/java/ch/vaudoise/apifactory/dto/ClientPersonResponse.java
@@ -1,12 +1,16 @@
 package ch.vaudoise.apifactory.dto;
 
+import ch.vaudoise.apifactory.entities.Contract;
+
 import java.util.Date;
+import java.util.List;
 
 public record ClientPersonResponse(
                                    Long id,
                                    String name,
                                    String email,
                                    String type,
-                                   Date birthdate                     // For PERSON
+                                   Date birthdate,                     // For PERSON
+                                   List<ContractRequest> listOfContracts
 ) implements ClientResponse {
 }
